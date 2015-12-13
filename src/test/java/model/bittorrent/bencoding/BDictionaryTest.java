027d1c49b35f10ebc61e9250ba3b572858fb0823
Adrien Lacroix <oni.dev.box@gmail.com>
2015-12-13 23:57:29 +0100

    Add test to Dictionary

--- src/test/java/model/bittorrent/bencoding/BDictionaryTest.java
+++ src/test/java/model/bittorrent/bencoding/BDictionaryTest.java
@@ -3,7 +3,9 @@ package model.bittorrent.bencoding;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,6 +22,18 @@ public class BDictionaryTest {
 		d.put(new BString("cow"), new BString("moo"));
 		BType t = new BDictionary(d);
 		Assert.assertEquals("d3:foo3:bar3:cow3:mooe", t.getBencodedValue());
+	}
+
+	@Test
+	public void testList() throws Exception {
+		Map<BType, BType> d = new LinkedHashMap<>();
+		List<BType> l = new ArrayList<>();
+		l.add(new BString("b"));
+		l.add(new BString("a"));
+		l.add(new BString("r"));
+		d.put(new BString("foo"), new BList(l));
+		BType t = new BDictionary(d);
+		Assert.assertEquals("d3:fool1:b1:a1:ree", t.getBencodedValue());
 	}
 
 	@Test
